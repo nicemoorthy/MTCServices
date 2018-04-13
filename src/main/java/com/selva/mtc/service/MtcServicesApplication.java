62b7b13ea9d38de0442f54aded17bd03f3511236
Selvamoorthy Kamaraj <selvamoorthy.kamaraj@gmail.com>
2018-04-13 09:46:32 -0500

    initial commit

---
+++ src/main/java/com/selva/mtc/service/MtcServicesApplication.java
@@ -0,0 +1,12 @@
+package com.selva.mtc.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtcServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtcServicesApplication.class, args);
+	}
+}
---
+++ src/test/java/com/selva/mtc/service/MtcServicesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.selva.mtc.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MtcServicesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
